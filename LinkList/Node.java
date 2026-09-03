2e4a699fbfb8be4be5987c6e0817a169f8e7484b
kulokulo-sora <woshiliuyangtian@gmail.com>
2026-09-03 16:57:01 +0800

    Add Linked list node

---
+++ LinkList/Node.java
@@ -0,0 +1,14 @@
+package LinkList;
+
+public class Node {
+    int val;
+    Node next;
+
+    public Node(int val){
+        this.val = val;
+    }
+    public Node(int val,Node next){
+        this.val = val;
+        this.next = next;
+    }
+}
